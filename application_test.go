30530233ec29434b763398f961aeeb89f27f91df
Timo Reimann <ttr314@googlemail.com>
2016-10-12 00:30:01 +0200

    Cover all code paths of WaitForApplication.

--- application_test.go
+++ application_test.go
@@ -488,29 +488,45 @@ func TestWaitOnApplication(t *testing.T) {
 	waitTime := 100 * time.Millisecond
 
 	tests := []struct {
-		desc    string
-		timeout time.Duration
-		appName string
+		desc          string
+		timeout       time.Duration
+		appName       string
+		testScope     string
+		shouldSucceed bool
 	}{
 		{
-			desc:    "existing app / timeout > ticker",
-			timeout: 200 * time.Millisecond,
-			appName: fakeAppName,
+			desc:          "initially existing app",
+			timeout:       0,
+			appName:       fakeAppName,
+			shouldSucceed: true,
+		},
+
+		{
+			desc:          "delayed existing app | timeout > ticker",
+			timeout:       200 * time.Millisecond,
+			appName:       fakeAppName,
+			testScope:     "wait-on-app",
+			shouldSucceed: true,
+		},
+
+		{
+			desc:          "delayed existing app | timeout < ticker",
+			timeout:       50 * time.Millisecond,
+			appName:       fakeAppName,
+			testScope:     "wait-on-app",
+			shouldSucceed: false,
 		},
 		{
-			desc:    "missing app / timeout > ticker",
-			timeout: 200 * time.Millisecond,
-			appName: "no_such_app",
+			desc:          "missing app | timeout > ticker",
+			timeout:       200 * time.Millisecond,
+			appName:       "no_such_app",
+			shouldSucceed: false,
 		},
 		{
-			desc:    "existing app / timeout < ticker",
-			timeout: 50 * time.Millisecond,
-			appName: fakeAppName,
-		},
-		{
-			desc:    "missing app / timeout < ticker",
-			timeout: 50 * time.Millisecond,
-			appName: "no_such_app",
+			desc:          "missing app | timeout < ticker",
+			timeout:       50 * time.Millisecond,
+			appName:       "no_such_app",
+			shouldSucceed: false,
 		},
 	}
 
@@ -519,6 +535,9 @@ func TestWaitOnApplication(t *testing.T) {
 		defaultConfig.PollingWaitTime = waitTime
 		configs := &configContainer{
 			client: &defaultConfig,
+			server: &serverConfig{
+				scope: test.testScope,
+			},
 		}
 
 		endpoint := newFakeMarathonEndpoint(t, configs)
@@ -530,7 +549,7 @@ func TestWaitOnApplication(t *testing.T) {
 		}()
 		timer := time.NewTimer(400 * time.Millisecond)
 		<-timer.C
-		if test.appName == fakeAppName {
+		if test.shouldSucceed {
 			assert.NoError(t, err, test.desc)
 		} else {
 			assert.IsType(t, err, ErrTimeoutError, test.desc)
